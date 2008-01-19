cc37c61cab66e71dcb1679932ad8b04e06463363
Takuya Noguchi <tnoguchi@users.noreply.github.com>
2008-01-19 04:59:18 +0000

    initial commit: just `rails`

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'f95fe5fc23b73e6490d69f3dda9f9b9a'
+end
