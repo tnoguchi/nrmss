a48e02de7d68e2ed0a6225b45ef116b0afabb051
Takuya Noguchi <tnoguchi@users.noreply.github.com>
2008-05-31 11:19:03 +0000

    Add index generator into IframeGenerator.

--- lib/iframe_generator.rb
+++ lib/iframe_generator.rb
@@ -43,7 +43,7 @@ class IframeGenerator
         item_identifier = item.url.sub(/\A.*?rakuten\...\.jp\/?/, '').sub(/\/\Z/, '').gsub('/', '_')
         item.groups.each do |group|
           suffix = (!group.is_a? Array || group.size == 1) ? '' : "_#{group.id}"
-          regulated_name = NKF.nkf('-We -m0', item.name).gsub(/[ 　]+/, ' ').tr("［］０-９ａ-ｚＡ-Ｚ", "[]0-9a-zA-Z")
+          regulated_name = NKF.nkf('-We -m0', item.name.gsub(/[ 　]+/, ' ').tr("［］０-９ａ-ｚＡ-Ｚ", "[]0-9a-zA-Z"))
           open(File.join(self.base_path, "#{item_identifier}#{suffix}.html"), "w") do |f|
             f.puts <<-EOS
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -62,6 +62,32 @@ class IframeGenerator
             EOS
           end
         end
+      end
+      open(File.join(self.base_path, "index.html"), "w") do |f|
+        f.puts <<-EOS
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="ja-JP" xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-JP" />
+<meta http-equiv="Content-Style-Type" content="text/css">
+<meta name="keywords" content="#{Nrmss.config['iframe_keywords']}" />
+<title>#{NKF.nkf('-We -m0', '関連商品リスト')}</title>
+<link href="#{items.first.url.sub(%r{//item.rakuten.co.jp/([^/]+)/([^/])+/}, "//www.rakuten.ne.jp/gold/\\1/")}css/related_items.css" media="screen" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<h1>#{NKF.nkf('-We -m0', '関連商品リスト')}</h1>
+<ul>
+        EOS
+        items.each do |item|
+          regulated_name = NKF.nkf('-We -m0', item.name.gsub(/[ 　]+/, ' ').tr("［］０-９ａ-ｚＡ-Ｚ", "[]0-9a-zA-Z") + 'の関連商品')
+          item_identifier = item.url.sub(/\A.*?rakuten\...\.jp\/?/, '').sub(/\/\Z/, '').gsub('/', '_')
+          f.puts %Q|<li><a href="#{item_identifier}.html">#{regulated_name}</a></li>|
+        end
+        f.puts <<-EOS
+</ul>
+</body>
+</html>
+        EOS
       end
     end
  
